de4e9048ec9b9c451d10da49856c946e3a5527f7
Maoyunlong <1026225160@qq.com>
2024-07-30 20:03:35 +0800

    更新

--- springboot/src/main/java/demo/mapper/TravelMapper.java
+++ springboot/src/main/java/demo/mapper/TravelMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface TravelMapper {
     @Select("SELECT gh AS employeeID, qsrq1 AS start_date, jzrq1 AS end_date, cclx AS travelType, sy AS businessReason, ydjp AS isOrderAirTicket, zwdlr AS WorkAgent, xxdd AS city, ccry AS participant from formtable_main_17 where requestid = #{requestId}")
-    public TravelInfo getTravelInfo(@Param("requestId") int requestId);
+    TravelInfo getTravelInfo(@Param("requestId") int requestId);
     @Select("select gh from formtable_main_17 where requestid = #{requestId}")
-    public String getGH(@Param("requestId") int requestId);
+    String getGH(@Param("requestId") int requestId);
 }
