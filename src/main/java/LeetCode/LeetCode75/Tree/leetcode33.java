acf543826e3a3609e4cef85ae89c2d47fe95febc
Maoyunlong <1026225160@qq.com>
2025-02-14 18:37:12 +0800

    树

---
+++ src/main/java/LeetCode/LeetCode75/Tree/leetcode33.java
@@ -0,0 +1,16 @@
+package LeetCode.LeetCode75.Tree;
+
+public class leetcode33 {
+    public static void main(String[] args) {
+        leetcode33 leetcode33 = new leetcode33();
+        System.out.println();
+    }
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left,right) + 1;
+    }
+}
---
+++ src/main/java/LeetCode/LeetCode75/Tree/leetcode34.java
@@ -0,0 +1,48 @@
+package LeetCode.LeetCode75.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class leetcode34 {
+    public static void main(String[] args) {
+        leetcode34 leetcode34 = new leetcode34();
+        TreeNode treeNode1 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        TreeNode treeNode2 = new TreeNode(1, new TreeNode(2), new TreeNode(3));
+        System.out.println(leetcode34.leafSimilar(treeNode1, treeNode2));
+    }
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        getLeftVal(root1,list1);
+        getLeftVal(root2,list2);
+        if (list1.size() == list2.size()){
+            for (int i = 0; i < list2.size(); i++) {
+                if (!Objects.equals(list2.get(i), list1.get(i))){
+                    return false;
+                }
+            }
+            return true;
+        }
+//        System.out.println("list1:"+list1);
+//        System.out.println("list2:"+list2);
+        return false;
+    }
+    public int getLeft(TreeNode treeNode){
+        if (treeNode.left == null && treeNode.right == null){
+            return treeNode.val;
+        }
+        return 0;
+    }
+
+    public void getLeftVal(TreeNode treeNode, List<Integer> list) {
+        if (treeNode == null){
+            return ;
+        }
+        if (treeNode.left == null && treeNode.right == null){
+            list.add(treeNode.val);
+        }
+        getLeftVal(treeNode.left,list);
+        getLeftVal(treeNode.right,list);
+    }
+}
