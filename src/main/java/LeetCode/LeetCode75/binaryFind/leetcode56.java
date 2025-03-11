67d6d59fffd2a016b7ef41320856ba134fce55c9
Maoyunlong <1026225160@qq.com>
2025-03-11 17:45:47 +0800

    LeetCode75

---
+++ src/main/java/LeetCode/LeetCode75/binaryFind/leetcode56.java
@@ -0,0 +1,11 @@
+package LeetCode.LeetCode75.binaryFind;
+
+public class leetcode56 {
+    public static void main(String[] args) {
+        leetcode56 leetcode56 = new leetcode56();
+        System.out.println();
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+
+    }
+}
